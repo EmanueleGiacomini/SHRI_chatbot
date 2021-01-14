25a96a19b6335c3571611eba7f89154431fc9a58
Veronica <vulcano.1760405@studenti.uniroma1.it>
2021-01-14 20:03:55 +0100

    added new functions in kb_handler

--- agent/kb_handler.py
+++ agent/kb_handler.py
@@ -17,7 +17,7 @@ class KB:
         return data
 
     def save(self,d):
-         with open(f"./agent/kb/{datetime.now().strftime('%Y%m%d-%H%M%S')}_kb.json", 'x') as outfile:
+         with open(f"./agent/database/{datetime.now().strftime('%Y%m%d-%H%M%S')}_kb.json", 'x') as outfile:
             json.dump(d, outfile)
 
     def get_region(self,key,data):
@@ -29,6 +29,22 @@ class KB:
 
     def get_dato(self,key,data):
         return data[key]
+
+    def set_dato(self,color,key,word):
+        with open(f"./agent/database/kb_01.json") as json_file:
+            data = js.load(json_file)
+            N=len(data["zone"])
+            for i in range(N):
+                if data["zone"][i]['colore']==color:
+                    elem =  data["zone"][i]
+                    if word == "posso":
+                        elem[key] = True
+                    elif word == "non posso":
+                        elem[key] = False
+        with open(f"./agent/database/kb_01.json","w") as js_file:
+            js_file.write(js.dumps(data))
+
+
 
 
 
